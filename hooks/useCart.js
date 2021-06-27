757c6364b202fd2b97c0b10e37d8957c78ef71ce
Daniel <info@daniel-bird.co.uk>
2021-06-27 18:54:10 +0100

    Store cart state in local storage

--- hooks/useCart.js
+++ hooks/useCart.js
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from 'react';
+import { useState, useEffect, createContext, useContext } from 'react';
 import { initiateCheckout } from '../lib/payments';
 import products from '../products.json';
 
@@ -10,6 +10,19 @@ export const CartContext = createContext();
 
 export function useCartState() {
   const [cart, updateCart] = useState(defaultCart);
+
+  useEffect(() => {
+    const stateFromStorage = window.localStorage.getItem('spacejelly_cart');
+    const data = stateFromStorage && JSON.parse(stateFromStorage);
+    if (data) {
+      updateCart(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    const data = JSON.stringify(cart);
+    window.localStorage.setItem('spacejelly_cart', data);
+  }, [cart]);
 
   const cartItems = Object.keys(cart.products).map(key => {
     const product = products.find(({ id }) => `${id}` === `${key}`);
